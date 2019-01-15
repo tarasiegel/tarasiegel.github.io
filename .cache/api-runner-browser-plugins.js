d409153b311c1f48e9a782e678727896a35fca3c
Tara Siegel <tarasiegel@Taras-MBP.fios-router.home>
2019-01-15 09:21:47 -0500

    trying out some static generator

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]

b6df14caa1af0f36a319d305052a88af071175b2
Tara Siegel <tarafsiegel@gmail.com>
2019-01-28 21:06:03 -0500

    updates to package

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,12 +2,12 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
-  "component---src-templates-blog-post-js": () => import("/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
-  "component---src-pages-404-js": () => import("/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-about-js": () => import("/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-index-js": () => import("/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
+  "component---src-templates-blog-post-js": () => import("/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
+  "component---src-pages-404-js": () => import("/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-index-js": () => import("/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
-exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/.cache/data.json")
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/.cache/data.json")
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,10 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/node_modules/gatsby-plugin-offline/app-shell.js"))),
-  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/src/templates/blog-post.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/src/pages/404.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/src/pages/about.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/tarasiegel/Documents/PersonalCode/fullstackkitchen/src/pages/index.js")))
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/node_modules/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/src/templates/blog-post.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/src/pages/about.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/tarasiegel/Documents/Code Projects/fullstackkitchen/src/pages/index.js")))
 }
 

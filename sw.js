8cd34eeb1c14ce8c908524b3780aa1acdac5b071
Tara Siegel <tarafsiegel@gmail.com>
2020-01-25 11:30:14 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-62b8688b31c049217b77.js"
+    "url": "webpack-runtime-4d3febfa088d83418401.js"
   },
   {
     "url": "styles.3176dd8dd46211a034a8.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bca24243b40b2747a38b87b358aa4bd5"
+    "revision": "f9b6e36835b5cf305ddd8e52917a8482"
   },
   {
     "url": "component---src-pages-404-js-90c5ce72a968087543a4.js"
